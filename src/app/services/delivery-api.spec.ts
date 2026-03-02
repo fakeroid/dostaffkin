58d8eacfda8ca339561d0005e823cbe37cd85241
Ярослав Богоявленский <36155768+fakeroid@users.noreply.github.com>
2026-03-02 11:14:33 +0000

    на деплой

---
+++ src/app/services/delivery-api.spec.ts
@@ -0,0 +1,16 @@
+// import { TestBed } from '@angular/core/testing';
+
+// import { DeliveryApi } from './delivery-api';
+
+// describe('DeliveryApi', () => {
+//   let service: DeliveryApi;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(DeliveryApi);
+//   });
+
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
